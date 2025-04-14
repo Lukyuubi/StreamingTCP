9fe24e5d75adc772f4bf5754723297726c6205b8
Lucas <l202071@dac.unicamp.br>
2025-04-14 15:41:35 -0300

    Adicionando funcionalidades

--- client.c
+++ client.c
@@ -14,7 +14,8 @@
 #define PORT "8008"
 #define MAX 2000
 
-void menu_principal() {
+void menu_principal()
+{
     printf("\n===== Menu Principal =====\n");
     printf("1. Operacoes de Registro e Modificacao\n");
     printf("2. Operacoes de Consulta\n");
@@ -25,35 +26,47 @@ void menu_principal() {
 void menu_registro() {
     printf("\n--- Registro e Modificacao ---\n");
     printf("1. Cadastrar novo filme\n");
-    printf("2. Voltar ao menu principal\n");
-    printf("Escolha uma opcao: ");
-}
-
-void menu_consulta() {
-    printf("\n--- Consultas ---\n");
-    printf("1. Listar todos os filmes (ID e Titulo)\n");
-    printf("2. Listar informacoes completas de todos os filmes\n");
-    printf("3. Buscar filme por ID\n");
+    printf("2. Adicionar novo genero a um filme existente\n");
+    printf("3. Remover filme por ID\n");
     printf("4. Voltar ao menu principal\n");
     printf("Escolha uma opcao: ");
 }
 
-void func(int sockfd) {
+void menu_consulta()
+{
+    printf("\n--- Consultas ---\n");
+    printf("1. Listar todos os filmes (ID e Titulo)\n");
+    printf("2. Listar informacoes completas de todos os filmes\n");
+    printf("3. Buscar filme por ID\n");
+    printf("4. Listar filmes por genero\n");
+    printf("5. Voltar ao menu principal\n");
+    printf("Escolha uma opcao: ");
+}
+
+void func(int sockfd)
+{
     char buff[MAX];
-    int n;
     int opcao_menu = 0;
 
-    while (1) {
+    while (1)
+    {
         menu_principal();
         fgets(buff, MAX, stdin);
         opcao_menu = atoi(buff);
 
-        if (opcao_menu == 1) {
+        if (opcao_menu == 1)
+        {
             menu_registro();
             fgets(buff, MAX, stdin);
-            if (strncmp(buff, "1", 1) == 0) {
+            
+            // Cadastrar novo filme
+            if (strncmp(buff, "1", 1) == 0)
+            {
+                // Opção de cadastro no server.c
                 write(sockfd, "1\n", 2);
-                for (int i = 0; i < 4; i++) {
+                // Esperado Nome, Genero, Diretor e Ano
+                for (int i = 0; i < 4; i++)
+                {
                     bzero(buff, sizeof(buff));
                     read(sockfd, buff, sizeof(buff));
                     printf("%s", buff);
@@ -65,20 +78,65 @@ void func(int sockfd) {
                 read(sockfd, buff, sizeof(buff));
                 printf("%s\n", buff);
             }
-        } else if (opcao_menu == 2) {
+
+            // Adicionar novo genero a um filme existente
+            else if (strncmp(buff, "2", 1) == 0)
+            {
+                //Envia pelo socket a opção
+                write(sockfd, "8\n", 2);
+                bzero(buff, sizeof(buff));
+                read(sockfd, buff, sizeof(buff));
+                printf("%s", buff);
+                bzero(buff, sizeof(buff));
+                fgets(buff, MAX, stdin);
+                write(sockfd, buff, strlen(buff));
+
+                bzero(buff, sizeof(buff));
+                read(sockfd, buff, sizeof(buff));
+                printf("%s", buff);
+                bzero(buff, sizeof(buff));
+                fgets(buff, MAX, stdin);
+                write(sockfd, buff, strlen(buff));
+
+                bzero(buff, sizeof(buff));
+                read(sockfd, buff, sizeof(buff));
+                printf("%s\n", buff);
+            }
+            // Remoção de filme por ID
+            else if (strncmp(buff, "3", 1) == 0) {
+                // Envio da opção pelo socket
+                write(sockfd, "9\n", 2);
+                bzero(buff, sizeof(buff));
+                read(sockfd, buff, sizeof(buff));
+                printf("%s", buff);
+                bzero(buff, sizeof(buff));
+                fgets(buff, MAX, stdin);
+                write(sockfd, buff, strlen(buff));
+                bzero(buff, sizeof(buff));
+                read(sockfd, buff, sizeof(buff));
+                printf("%s\n", buff);
+            }
+        }
+        else if (opcao_menu == 2)
+        {
             menu_consulta();
             fgets(buff, MAX, stdin);
-            if (strncmp(buff, "1", 1) == 0) {
+            if (strncmp(buff, "1", 1) == 0)
+            {
                 write(sockfd, "4\n", 2);
                 bzero(buff, sizeof(buff));
                 read(sockfd, buff, sizeof(buff));
                 printf("%s\n", buff);
-            } else if (strncmp(buff, "2", 1) == 0) {
+            }
+            else if (strncmp(buff, "2", 1) == 0)
+            {
                 write(sockfd, "5\n", 2);
                 bzero(buff, sizeof(buff));
                 read(sockfd, buff, sizeof(buff));
                 printf("%s\n", buff);
-            } else if (strncmp(buff, "3", 1) == 0) {
+            }
+            else if (strncmp(buff, "3", 1) == 0)
+            {
                 write(sockfd, "6\n", 2);
                 // Lê o prompt do servidor: "Digite o ID do filme"
                 bzero(buff, sizeof(buff));
@@ -95,29 +153,50 @@ void func(int sockfd) {
                 read(sockfd, buff, sizeof(buff));
                 printf("%s\n", buff);
             }
-        } else if (opcao_menu == 3) {
+            else if (strncmp(buff, "4", 1) == 0)
+            {
+                write(sockfd, "7\n", 2);
+                bzero(buff, sizeof(buff));
+                read(sockfd, buff, sizeof(buff));
+                printf("%s", buff);
+                bzero(buff, sizeof(buff));
+                fgets(buff, MAX, stdin);
+                write(sockfd, buff, strlen(buff));
+                bzero(buff, sizeof(buff));
+                read(sockfd, buff, sizeof(buff));
+                printf("%s\n", buff);
+            }
+        }
+        else if (opcao_menu == 3)
+        {
             write(sockfd, "exit\n", 5);
             printf("Encerrando cliente...\n");
             break;
-        } else {
+        }
+        else
+        {
             printf("Opcao invalida. Tente novamente.\n");
         }
     }
 }
 
-void *get_in_addr(struct sockaddr *sa) {
-    if (sa->sa_family == AF_INET) return &(((struct sockaddr_in*)sa)->sin_addr);
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET)
+        return &(((struct sockaddr_in *)sa)->sin_addr);
+    return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int sockfd;
     struct addrinfo hints, *servinfo, *p;
     int rv;
     char s[INET6_ADDRSTRLEN];
 
-    if (argc != 2) {
-        fprintf(stderr,"Uso: %s <hostname>\n", argv[0]);
+    if (argc != 2)
+    {
+        fprintf(stderr, "Uso: %s <hostname>\n", argv[0]);
         exit(1);
     }
 
@@ -125,18 +204,22 @@ int main(int argc, char *argv[]) {
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
 
-    if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+    if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0)
+    {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
 
-    for (p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+    for (p = servinfo; p != NULL; p = p->ai_next)
+    {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+        {
             perror("client: socket");
             continue;
         }
 
-        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+        {
             close(sockfd);
             perror("client: connect");
             continue;
@@ -144,7 +227,8 @@ int main(int argc, char *argv[]) {
         break;
     }
 
-    if (p == NULL) {
+    if (p == NULL)
+    {
         fprintf(stderr, "client: falha ao conectar\n");
         return 2;
     }
--- server.c
+++ server.c
@@ -20,22 +20,28 @@
 
 sqlite3 *db;
 
-void sigchld_handler(int s) {
+void sigchld_handler(int s)
+{
     int saved_errno = errno;
-    while (waitpid(-1, NULL, WNOHANG) > 0);
+    while (waitpid(-1, NULL, WNOHANG) > 0)
+        ;
     errno = saved_errno;
 }
 
-void *get_in_addr(struct sockaddr *sa) {
-    if (sa->sa_family == AF_INET) return &(((struct sockaddr_in*)sa)->sin_addr);
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET)
+        return &(((struct sockaddr_in *)sa)->sin_addr);
+    return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
 
-void init_db() {
+void init_db()
+{
     char *err_msg = NULL;
 
     const char *drop_sql = "DROP TABLE IF EXISTS filmes;";
-    if (sqlite3_exec(db, drop_sql, 0, 0, &err_msg) != SQLITE_OK) {
+    if (sqlite3_exec(db, drop_sql, 0, 0, &err_msg) != SQLITE_OK)
+    {
         fprintf(stderr, "Erro ao remover tabela: %s\n", err_msg);
         sqlite3_free(err_msg);
         exit(1);
@@ -48,23 +54,29 @@ void init_db() {
                              "diretor TEXT NOT NULL,"
                              "ano INTEGER NOT NULL"
                              ");";
-    if (sqlite3_exec(db, create_sql, 0, 0, &err_msg) != SQLITE_OK) {
+    if (sqlite3_exec(db, create_sql, 0, 0, &err_msg) != SQLITE_OK)
+    {
         fprintf(stderr, "Erro ao criar tabela: %s\n", err_msg);
         sqlite3_free(err_msg);
         exit(1);
     }
 }
 
-void func(int connfd) {
+void func(int connfd)
+{
     char buff[MAX];
+    char msg[] = "Comando invalido.\n";
 
-    for (;;) {
+    for (;;)
+    {
         bzero(buff, MAX);
         read(connfd, buff, sizeof(buff));
 
-        if (strncmp("exit", buff, 4) == 0) break;
-
-        if (strncmp("1", buff, 1) == 0) {  // Cadastrar
+        if (strncmp("exit", buff, 4) == 0)
+            break;
+        // Cadastrar novo filme
+        if (strncmp("1", buff, 1) == 0)
+        { 
             char titulo[MAX], genero[MAX], diretor[MAX], ano_str[MAX];
             write(connfd, "Titulo:\n", 8);
             read(connfd, titulo, sizeof(titulo));
@@ -87,16 +99,19 @@ void func(int connfd) {
             sqlite3_finalize(stmt);
 
             // Envia ao cliente mensagem de que o filme foi corretamente cadastrado
-            char msg[] = "Filme cadastrado.\n";
+            char msg[] = "\nFilme cadastrado.\n";
             write(connfd, msg, sizeof(msg));
-
-        } else if (strncmp("4", buff, 1) == 0) {  // Listar todos
+        }
+        // Listar todos
+        else if (strncmp("4", buff, 1) == 0)
+        { 
             sqlite3_stmt *stmt;
             const char *sql = "SELECT id, titulo FROM filmes;";
             sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
 
             char response[MAX] = "";
-            while (sqlite3_step(stmt) == SQLITE_ROW) {
+            while (sqlite3_step(stmt) == SQLITE_ROW)
+            {
                 char row[MAX];
                 snprintf(row, MAX, "ID: %d | Titulo: %s\n",
                          sqlite3_column_int(stmt, 0),
@@ -104,16 +119,20 @@ void func(int connfd) {
                 strcat(response, row);
             }
             sqlite3_finalize(stmt);
-            strcat(response, "Digite a operacao desejada:\n");
+            strcat(response, "\nDigite a operacao desejada:\n");
             write(connfd, response, strlen(response));
         }
-        else if (strncmp("5", buff, 1) == 0) {  // Listar todas as informações dos filmes
+        // Lista todas as informações dos filmes
+        else if (strncmp("5", buff, 1) == 0)
+        { 
             sqlite3_stmt *stmt;
+            // Query
             const char *sql = "SELECT titulo, genero, diretor, ano FROM filmes;";
             sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
-        
             char response[MAX] = "";
-            while (sqlite3_step(stmt) == SQLITE_ROW) {
+
+            while (sqlite3_step(stmt) == SQLITE_ROW)
+            {
                 char row[MAX];
                 snprintf(row, MAX, "Titulo: %s | Genero: %s | Diretor: %s | Ano: %d\n",
                          sqlite3_column_text(stmt, 0),
@@ -123,45 +142,169 @@ void func(int connfd) {
                 strcat(response, row);
             }
             sqlite3_finalize(stmt);
-            strcat(response, "Digite a operacao desejada:\n");
+            strcat(response, "\nDigite a operacao desejada:\n");
             write(connfd, response, strlen(response));
         }
-        else if (strncmp("6", buff, 1) == 0) {  // Buscar filme por ID
+        // Buscar filme por ID
+        else if (strncmp("6", buff, 1) == 0)
+        { // Buscar filme por ID
             char id_str[MAX];
             int id;
             sqlite3_stmt *stmt;
-        
+
             write(connfd, "Digite o ID do filme:\n", 23);
             read(connfd, id_str, sizeof(id_str));
             id = atoi(id_str);
-        
+
             const char *sql = "SELECT titulo, genero, diretor, ano FROM filmes WHERE id = ?;";
             sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
             sqlite3_bind_int(stmt, 1, id);
-        
+
             char response[MAX] = "";
-            if (sqlite3_step(stmt) == SQLITE_ROW) {
+            if (sqlite3_step(stmt) == SQLITE_ROW)
+            {
                 snprintf(response, MAX,
                          "Titulo: %s\nGenero: %s\nDiretor: %s\nAno: %d\n",
                          sqlite3_column_text(stmt, 0),
                          sqlite3_column_text(stmt, 1),
                          sqlite3_column_text(stmt, 2),
                          sqlite3_column_int(stmt, 3));
-            } else {
+            }
+            else
+            {
                 snprintf(response, MAX, "Filme com ID %d nao encontrado.\n", id);
             }
-        
+
             sqlite3_finalize(stmt);
             write(connfd, response, strlen(response));
         }
-        
-        else {
-            write(connfd, "Comando invalido.\n", 45);
+        // Listar filmes por gênero
+        else if (strncmp("7", buff, 1) == 0)
+        { 
+            char genero[MAX];
+            sqlite3_stmt *stmt;
+
+            write(connfd, "Digite o genero desejado:\n", 27);
+            read(connfd, genero, sizeof(genero));
+
+            const char *sql = "SELECT titulo, genero, diretor, ano FROM filmes WHERE genero = ?;";
+            sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+            sqlite3_bind_text(stmt, 1, genero, -1, SQLITE_STATIC);
+
+            char response[MAX] = "";
+            int found = 0;
+            while (sqlite3_step(stmt) == SQLITE_ROW)
+            {
+                found = 1;
+                char row[MAX];
+                snprintf(row, MAX, "Titulo: %s | Genero: %s | Diretor: %s | Ano: %d\n",
+                         sqlite3_column_text(stmt, 0),
+                         sqlite3_column_text(stmt, 1),
+                         sqlite3_column_text(stmt, 2),
+                         sqlite3_column_int(stmt, 3));
+                strcat(response, row);
+            }
+
+            if (!found)
+            {
+                snprintf(response, MAX, "Nenhum filme encontrado com o genero informado.\n");
+            }
+
+            strcat(response, "\nDigite a operacao desejada:\n");
+            sqlite3_finalize(stmt);
+            write(connfd, response, strlen(response));
+        }
+        // Adicionar novo genero a um filme existente com o ID
+        else if (strncmp("8", buff, 1) == 0)
+        { 
+            char id_str[MAX], novo_genero[MAX];
+            int id;
+            sqlite3_stmt *stmt;
+
+            write(connfd, "Informe o ID do filme:\n", 24);
+            read(connfd, id_str, sizeof(id_str));
+            id = atoi(id_str);
+
+            // Verifica se o filme existe
+            const char *check_sql = "SELECT genero FROM filmes WHERE id = ?;";
+            sqlite3_prepare_v2(db, check_sql, -1, &stmt, NULL);
+            sqlite3_bind_int(stmt, 1, id);
+
+            char genero_atual[MAX] = "";
+            if (sqlite3_step(stmt) == SQLITE_ROW)
+            {
+                strncpy(genero_atual, (const char *)sqlite3_column_text(stmt, 0), MAX);
+                sqlite3_finalize(stmt);
+
+                write(connfd, "Informe o novo genero a adicionar:\n", 36);
+                read(connfd, novo_genero, sizeof(novo_genero));
+
+                genero_atual[strcspn(genero_atual, "\n")] = 0;
+                novo_genero[strcspn(novo_genero, "\n")] = 0;
+
+                // Concatena novo gênero à lista
+                char genero_final[MAX] = "";
+                strncat(genero_final, genero_atual, MAX - strlen(genero_final) - 1);
+                strncat(genero_final, ", ", MAX - strlen(genero_final) - 1);
+                strncat(genero_final, novo_genero, MAX - strlen(genero_final) - 1);
+
+                // Atualiza o campo no banco
+                const char *update_sql = "UPDATE filmes SET genero = ? WHERE id = ?;";
+                sqlite3_prepare_v2(db, update_sql, -1, &stmt, NULL);
+                sqlite3_bind_text(stmt, 1, genero_final, -1, SQLITE_STATIC);
+                sqlite3_bind_int(stmt, 2, id);
+                sqlite3_step(stmt);
+                sqlite3_finalize(stmt);
+
+                const char *msg_ok = "\nGenero adicionado com sucesso.\n";
+                write(connfd, msg_ok, strlen(msg_ok));
+
+            } 
+            else if (strncmp("9", buff, 1) == 0) {  // Remover filme por ID
+                char id_str[MAX];
+                int id;
+                sqlite3_stmt *stmt;
+            
+                write(connfd, "Informe o ID do filme a remover:\n", 34);
+                read(connfd, id_str, sizeof(id_str));
+                id = atoi(id_str);
+            
+                const char *check_sql = "SELECT 1 FROM filmes WHERE id = ?;";
+                sqlite3_prepare_v2(db, check_sql, -1, &stmt, NULL);
+                sqlite3_bind_int(stmt, 1, id);
+            
+                if (sqlite3_step(stmt) == SQLITE_ROW) {
+                    sqlite3_finalize(stmt);
+                    const char *delete_sql = "DELETE FROM filmes WHERE id = ?;";
+                    sqlite3_prepare_v2(db, delete_sql, -1, &stmt, NULL);
+                    sqlite3_bind_int(stmt, 1, id);
+                    sqlite3_step(stmt);
+                    sqlite3_finalize(stmt);
+                    const char *msg_ok = "\nFilme removido com sucesso.\n"; 
+                    write(connfd, msg_ok, strlen(msg_ok));
+                } else {
+                    sqlite3_finalize(stmt);
+                    const char *msg_err = "\nFilme nao encontrado.\n\n";
+                    write(connfd, msg_err, strlen(msg_err));
+                }
+            }            
+            else
+            {
+                sqlite3_finalize(stmt);
+                const char *msg_erro = "\nFilme nao encontrado.\n";
+                write(connfd, msg_erro, strlen(msg_erro));
+            }
+        }
+
+        else
+        {
+            write(connfd, msg, strlen(msg));
         }
     }
 }
 
-int main(void) {
+int main(void)
+{
     struct addrinfo hints, *servinfo, *p;
     struct sockaddr_storage their_addr;
     socklen_t sin_size;
@@ -178,21 +321,26 @@ int main(void) {
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0)
+    {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
 
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+    for (p = servinfo; p != NULL; p = p->ai_next)
+    {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+        {
             perror("server: socket");
             continue;
         }
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+        {
             perror("setsockopt");
             exit(1);
         }
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+        {
             close(sockfd);
             perror("server: bind");
             continue;
@@ -202,12 +350,14 @@ int main(void) {
 
     freeaddrinfo(servinfo);
 
-    if (p == NULL) {
+    if (p == NULL)
+    {
         fprintf(stderr, "server: failed to bind\n");
         exit(1);
     }
 
-    if (listen(sockfd, BACKLOG) == -1) {
+    if (listen(sockfd, BACKLOG) == -1)
+    {
         perror("listen");
         exit(1);
     }
@@ -215,17 +365,20 @@ int main(void) {
     sa.sa_handler = sigchld_handler;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = SA_RESTART;
-    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+    if (sigaction(SIGCHLD, &sa, NULL) == -1)
+    {
         perror("sigaction");
         exit(1);
     }
 
     printf("Servidor aguardando conexoes na porta %s...\n", PORT);
 
-    while (1) {
+    while (1)
+    {
         sin_size = sizeof their_addr;
         new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-        if (new_fd == -1) {
+        if (new_fd == -1)
+        {
             perror("accept");
             continue;
         }
@@ -233,7 +386,8 @@ int main(void) {
         inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
         printf("Servidor: conexao de %s\n", s);
 
-        if (!fork()) {
+        if (!fork())
+        {
             close(sockfd);
             func(new_fd);
             close(new_fd);
